791faabe7c5a2b0fc347b8b50dfb0e3f1768925f
kobirul5 <kobirul05j@gmail.com>
2024-10-03 23:12:14 +0600

    added button section

--- js/video.js
+++ js/video.js
@@ -1,3 +1,18 @@
+function getTimeString(time) {
+    const hour = parseInt(time / 3600);
+    const remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    const second = remainingSecond % 60
+
+    return (`${hour} hour ${minute} minute ${second} second`)
+
+}
+
+
+
+
+
+
 // fetch section 
 
 const loadCategory = () => {
@@ -11,7 +26,14 @@ const videosCategory = () => {
         .then(data => displayVideo(data.videos))
 }
 
-
+// btn color remove 
+const removeBtnColor = () => {
+    const buttons = document.getElementsByClassName('category-btn')
+    for(const button of buttons){
+        button.classList.remove("bg-red", "text-white")
+    }
+}
+//  demoooooo
 const demoObject = {
     "category_id": "1001",
     "video_id": "aaaa",
@@ -31,11 +53,42 @@ const demoObject = {
     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
 }
 
+const loadVideo = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            removeBtnColor()
+            const activeBtn = document.getElementById(`btn-${id}`)
+            activeBtn.classList.add("bg-red", "text-white")
+            displayVideo(data.category)
+        })
+        .catch((error) => console.log(error))
+}
+
+
+
 
 const displayVideo = (videos) => {
     const videosContainer = document.getElementById('videos')
+    videosContainer.innerHTML = ""
+
+    if(videos.length === 0){
+        videosContainer.classList.remove('grid')
+        videosContainer.innerHTML = `
+        <div class="main-h-[300px] flex flex-col justify-center items-center gap-5">
+        <img src="./assets/icon.png" />
+        <h2 class ="text-2xl font-bold text-center ">
+        No content in this category
+        </h2>
+        </div>
+        `
+    }
+    else{
+        videosContainer.classList.add('grid')
+    }
+
     videos.forEach(video => {
-        console.log(video)
+
         const card = document.createElement('div');
         card.classList = "card card-compact"
         card.innerHTML = `
@@ -44,7 +97,10 @@ const displayVideo = (videos) => {
             class = "h-full w-full object-cover"
             src="${video.thumbnail}"
             alt="video" />
-            <span class=" absolute bottom-4 right-5">${video.others.posted_date}</span>
+            ${video.others.posted_date?.length === 0 ? '' : `<span class=" absolute bg-black p-2 text-white text-xs bottom-4 right-5"> ${getTimeString(video.others.posted_date)}
+            </span>`
+            }
+            
         </figure>
         <div class="py-2 flex gap-2">
             <div>
@@ -54,15 +110,14 @@ const displayVideo = (videos) => {
             <h2 class="font-bold">${video.title} </h2>
             <div class =" flex gap-3">
             <p class= "text-gray-500">${video.authors[0].profile_name} </p>
-            ${
-                video.authors[0].verified === true ? `<img class =" w-5 object-cover" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ''
+            ${video.authors[0].verified === true ? `<img class =" w-5 object-cover" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ''
             }
             </div>
             <p class= "text-gray-500">${video.others.views} views</p>
             </div>
         </div>
             `
-    videosContainer.append(card)
+        videosContainer.append(card)
     })
 }
 // category
@@ -72,13 +127,17 @@ const displayVideo = (videos) => {
 // : 
 // "1001"
 
+
 const displayCategory = (categories) => {
     const categoriesContainer = document.getElementById('categories-container')
     categories.forEach(item => {
-        const button = document.createElement('button')
-        button.classList = "btn"
-        button.innerText = item.category;
-        categoriesContainer.append(button);
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = `
+        <button id="btn-${item.category_id}" class="btn category-btn" onclick="loadVideo(${item.category_id})"> ${item.category}</button>
+        `
+        // button.classList = "btn"
+        // button.innerText = item.category;
+        categoriesContainer.append(buttonContainer);
     });
 }
 loadCategory()
